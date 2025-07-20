d86ad47726e3374d8b0ce6a935a40b1bd44adad1
Steponass <steponas.dabuzinskas@gmail.com>
2025-07-20 08:28:19 +0300

    1. Added fonts.css. 2. Jost is our font. Variable, sharp v. Foundry: Indestructible Type. Appropriate 😁

--- HammeringStuff/src/App.tsx
+++ HammeringStuff/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "components/layout/Header";
 import TestGame from "games/TestGame";
-import "./App.css";
+import "./styles/fonts.css"
 
 const App: React.FC = () => {
   return (
